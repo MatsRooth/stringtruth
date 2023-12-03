faca12fa78e803217e2219a2ebef8ccb8bfe08c9
Mats <mr249@cornell.edu>
2023-12-03 09:47:17 -0500

    benchmark/cl23.txt

--- script/loop_text_and_truth.py
+++ script/loop_text_and_truth.py
@@ -1,6 +1,7 @@
 import sys
 import warnings
-
+# Demo a generator function for the benchmark
+# It yields 3477 items
 
 # python3 script/loop_text_and_truth.py data_clean/pooled_text data_clean/pooled_truth
 
@@ -24,10 +25,22 @@ truth = {z.strip().split(" ")[0]:z.strip().split(" ")[1:] for z in open(truth_fi
 # Dictionary from uid to dictionary from string to 't', 'f' or 'u'
 truth = {z.strip().split(" ")[0]:dict(zip(*(iter(z.strip().split(" ")[1:]),) * 2)) for z in open(truth_file)}
 
-for u in uid:
-    print(f'{u} {text[u]}')
-    for s in truth[u].keys():
-        print(f'  {s} {truth[u][s]}')
+# Generator function for items in the benchmark, in a tuple format like
+#  ('jack_a_14', 'lool', 'f')
+#  ('ava_a_5', 'aTqF', 't')
+
+def benchmark():
+    for u in uid:
+        for s in truth[u].keys():
+            yield (u,text[u],s,truth[u][s])
+
+
+for x in benchmark(): print(x)
+           
+#for u in uid:
+#    print(f'{u} {text[u]}')
+#    for s in truth[u].keys():
+#        print(f' {text[u]} {s} {truth[u][s]}')
 
 
 
