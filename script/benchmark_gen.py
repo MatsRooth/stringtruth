d67905bdb77ad49743ad8532f9e1fe3112d26483
Mats <mr249@cornell.edu>
2023-12-04 11:29:28 -0500

    update script/benchmark_gen.py

--- script/benchmark_gen.py
+++ script/benchmark_gen.py
@@ -3,7 +3,7 @@ from ast import literal_eval
 
 benchmark_file =  sys.argv[1]
 
-# This is iterable
+# This is iterabl
 benchmark =  map(lambda z:literal_eval(z), open(benchmark_file))
 
 # Print 8 items
@@ -18,3 +18,27 @@ benchmark =  map(lambda z:literal_eval(z), open(benchmark_file))
 benchmark_by_5 = zip(*(benchmark,) * 5)
 
 for x in zip(range(3),benchmark_by_5): print(x[1])
+
+# Experiment generating a Dialog from a tuple
+def tuple2dialog(x):
+    systemprompt = {"role": "system", "content": "Always answer with a single word 'True' or 'False'"}
+    userprompt = {"role": "user", "content": f"Consider the string '{x[2]}'. True or False: {x[1]}"}
+    return [systemprompt,userprompt]
+
+print('===================')
+for x in zip(range(3),benchmark): print(tuple2dialog(x[1]))
+
+print('===================')
+
+def gen_dialogs():
+	for x in zip(range(3),benchmark_by_5):
+            dialog = map(tuple2dialog,x[1])
+            yield list(dialog)
+
+for dialogs in gen_dialogs(): print(dialogs)
+
+
+
+
+
+
